d80e525a63038f7ebe043cd401278fa1eb0194a6
Notebook-Bruno <sbruno25@hotmail.com>
2025-05-17 18:08:15 -0300

    update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = 3000;
 
 app.use(cors());
 
-const API_URL = "http://100.106.19.113:3000/data"; // Substitua pela URL da API real http://api.binance.com
+const API_URL = "https://testnet.binance.vision"; // Substitua pela URL da API real http://api.binance.com
 const SYMBOL = "BTCUSDT";
 const QUANTITY = "0.00020563"; // Aproximadamente 100,00
 const PERIOD = 14;
